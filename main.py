0cc0d79c1172d0409c2cf83de44edb903fe30809
Maxence7595 <Maxence.amirkhanian@hotmail.com>
2026-03-13 21:17:31 +0100

    feat: creation du point d'entree main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+"""
+Jeu de Devinette - Nombre Mystere
+Point d'entree principal du jeu
+
+Auteur: Maxence MUSIC
+"""
+
+from menu import afficher_titre, afficher_menu_principal, afficher_regles, choisir_difficulte
+from game import jouer_partie
+from utils import afficher_score
+
+
+def main():
+"""Fonction principale du jeu"""
+afficher_titre()
+while True:
+choix = afficher_menu_principal()
+if choix == "1":
+niveau = choisir_difficulte()
+tentatives = jouer_partie(niveau)
+afficher_score(tentatives, niveau)
+elif choix == "2":
+afficher_regles()
+elif choix == "3":
+print("\nMerci d'avoir joue! A bientot!")
+break
+else:
+print("Choix invalide. Veuillez reessayer.")
+
+
+if __name__ == "__main__":
+main()
